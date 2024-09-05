283b9a57117fb1e2103caad570afd40b53088aa9
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-09-05 19:35:59 +0530

    Create sample data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const path = require('path')
 const mongoose = require('mongoose')
-const app = express();
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -11,7 +11,8 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const path = require('path')
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
