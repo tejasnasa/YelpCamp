e2f95208bad40d8acd38b88468f5bc885e6e74d0
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-14 17:22:41 +0530

    Add reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema } = require("./schemas.js");
+const Review = require("./models/review.js");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -90,6 +91,18 @@ app.delete("/campgrounds/:id", async (req, res) => {
   await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds");
 });
+
+app.post(
+  "/campgrounds/:id/reviews",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
